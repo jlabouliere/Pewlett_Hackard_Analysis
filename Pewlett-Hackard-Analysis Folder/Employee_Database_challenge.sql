31b9b7b079e54298bc53c80c63831980fe626d49
jlabouliere <98665941+jlabouliere@users.noreply.github.com>
2022-05-22 15:26:07 -0500

    Completed challenge sql

--- Pewlett-Hackard-Analysis Folder/Employee_Database_challenge.sql
+++ Pewlett-Hackard-Analysis Folder/Employee_Database_challenge.sql
@@ -10,3 +10,39 @@ INNER JOIN titles as ti
 ON (e.emp_no = ti.emp_no)
 WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 ORDER BY emp_no;
+
+-- Use Dictinct with Orderby to remove duplicate rows
+SELECT DISTINCT ON (rt.emp_no) rt.emp_no,
+rt.first_name,
+rt.last_name,
+rt.title
+
+INTO unique_titles
+FROM retirement_titles AS rt
+WHERE rt.to_date = ('9999-01-01')
+ORDER BY rt.emp_no, rt.to_date DESC;
+
+-- Retrieve the number of employees by their most recent job title who are about to retire
+SELECT COUNT(ut.emp_no),ut.title
+INTO retiring_titles
+FROM unique_titles as ut
+GROUP BY title 
+ORDER BY COUNT(title) DESC;
+
+-- Create a Mentorship Eligibility table that holds the employees who are eligible to participate in a mentorship program
+SELECT DISTINCT ON(e.emp_no) e.emp_no,
+e.first_name,
+e.last_name,
+e.birth_date,
+de.from_date,
+de.to_date,
+ti.title
+INTO mentorship_eligibility
+FROM employees AS e
+INNER JOIN dept_emp AS de
+ON (e.emp_no = de.emp_no)
+INNER JOIN titles AS ti
+ON (e.emp_no = ti.emp_no)
+WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+AND (de.to_date = '9999-01-01')
+ORDER BY emp_no;
